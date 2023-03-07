9b553c84e1d0b6cb1ae2cce58873f96d6a694cb1
erico <ecocaval@hotmail.com>
2023-03-06 22:07:14 -0300

    Fix: enviroment variables

--- src/config/database.connection.js
+++ src/config/database.connection.js
@@ -7,9 +7,14 @@ dotenv.config();
 const { Pool } = pg;
 
 const configDatabase = {
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ...(process.env.NODE_ENV === "production" && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
 };
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+const connection = new Pool(configDatabase);
 
-export const db = new Pool(configDatabase);
+export default connection;
