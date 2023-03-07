6b3c0442c55416a6cce72ac8a57103e2629be0e5
erico <ecocaval@hotmail.com>
2023-03-06 21:04:02 -0300

    Fix: correcting app.js

--- src/app.js
+++ src/app.js
@@ -8,10 +8,6 @@ const PORT = 5000
 app.use(express.json())
 app.use(cors())
 
-app.use(gamesRouter)
-app.use(customerRouter)
-app.use(rentalsRouter)
-
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
 })
