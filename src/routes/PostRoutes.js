c7360185e78b4343ae820ca26b7fc719152d763f
“gsnorberto” <sn_gabriel@outlook.com>
2023-03-08 21:08:52 -0300

    feat: get posts

--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { publishPost } from '../controllers/PostController.js'
+import { getPosts, publishPost } from '../controllers/PostController.js'
 import { validateSchema } from '../middlewares/ValidateSchema.js'
 import { postSchema } from '../schemas/PostSchema.js'
 
 const postRouter = Router()
 
 postRouter.post("/posts/new", validateSchema(postSchema), publishPost)
+postRouter.get("/posts", getPosts)
 
 export default postRouter
