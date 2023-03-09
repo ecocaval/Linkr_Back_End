280fefdc6ec355af644e3915f962d257189410e1
erico <ecocaval@hotmail.com>
2023-03-09 20:17:56 -0300

    Feat: expanding validate Token

--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { getPosts, publishPost } from '../controllers/PostController.js'
 import { validateSchema } from '../middlewares/ValidateSchema.js'
+import { validateToken } from '../middlewares/ValidateToken.js'
 import { postSchema } from '../schemas/PostSchema.js'
 
 const postRouter = Router()
 
-postRouter.post("/posts/new", validateSchema(postSchema), publishPost)
-postRouter.get("/posts", getPosts)
+postRouter.post("/posts/new", validateToken, validateSchema(postSchema), publishPost)
+postRouter.get("/posts", validateToken, getPosts)
 
 export default postRouter
