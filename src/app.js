bcfd3be3623d63a357f1846b0c5abeb5aad6cb31
erico <ecocaval@hotmail.com>
2023-03-10 03:24:09 -0300

    Feat: adding get hashtags route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 //* Libraries
 import express from "express"
 import cors from "cors"
+
+//* Routes
 import authRouter from "./routes/AuthRoutes.js"
 import postRouter from "./routes/PostRoutes.js"
 import userRouter from "./routes/UserRoutes.js"
+import hashtagRouter from "./routes/HashtagRoutes.js"
 
 const app = express()
 const PORT = 5000
@@ -11,7 +14,7 @@ const PORT = 5000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use([authRouter, postRouter, userRouter])
+app.use([authRouter, postRouter, userRouter, hashtagRouter])
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
