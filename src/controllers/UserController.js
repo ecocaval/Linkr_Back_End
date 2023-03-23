ad1711c2128851fab059247d360a5faedcb45719
denilson <denilsondslima@gmail.com>
2023-03-23 19:23:42 -0300

    feat: add follow function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-import { selectUserById, selectUsers } from "../repositories/UserRepository.js"
+import { deleteFollowUser, insertFollowUser, selectFollow, selectUserById, selectUsers } from "../repositories/UserRepository.js"
 
 export async function getUsers(req, res) {
     const { userId } = req.locals
@@ -20,6 +20,32 @@ export async function getMyUser(req, res) {
     try {
         const user = await selectUserById(userId)
         res.status(200).send(user.rows[0])
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function followUser(req, res) {
+    const {follower_id, followed_id } = req.body;
+    try {
+        const {rowCount} = await selectFollow(follower_id, followed_id)
+        if(rowCount > 0 || followed_id === follower_id) return res.sendStatus(409)
+        
+        await insertFollowUser(follower_id, followed_id)
+        res.send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+export async function deleteFollow(req, res) {
+    const {follower_id, followed_id } = req.body;
+    try {
+        const {rowCount} = await selectFollow(follower_id, followed_id)
+        if(rowCount === 0) return res.sendStatus(409)
+
+        await deleteFollowUser(follower_id, followed_id)
+        res.sendStatus(204)
     } catch (error) {
         res.status(500).send(error)
     }
