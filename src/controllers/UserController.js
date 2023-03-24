de6e6e4d527d5b511eec53af5cf71adb722e34f9
denilson <denilsondslima@gmail.com>
2023-03-23 22:28:40 -0300

    fix: button follow

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,52 +1,87 @@
-import { deleteFollowUser, insertFollowUser, selectFollow, selectUserById, selectUsers } from "../repositories/UserRepository.js"
+import {
+  deleteFollowUser,
+  insertFollowUser,
+  selectFollow,
+  selectUserById,
+  selectUsers,
+} from "../repositories/UserRepository.js";
 
 export async function getUsers(req, res) {
-    const { userId } = req.locals
-    const { id } = req.query
-    try {
-        if (id) {
-            const { rows: [user] } = await selectUserById(id)
-            return res.status(200).send(user)
-        }
-        const { rows: users } = await selectUsers(userId)
-        res.status(200).send(users)
-    } catch (error) {
-        res.status(500).send(error)
+  const { userId } = req.locals;
+  const { id } = req.query;
+  try {
+    if (id) {
+      const {
+        rows: [user],
+      } = await selectUserById(id);
+      return res.status(200).send(user);
     }
+    const { rows: users } = await selectUsers(userId);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 export async function getMyUser(req, res) {
-    const { userId } = req.locals
-    try {
-        const user = await selectUserById(userId)
-        res.status(200).send(user.rows[0])
-    } catch (error) {
-        res.status(500).send(error)
-    }
+  const { userId } = req.locals;
+  try {
+    const user = await selectUserById(userId);
+    res.status(200).send(user.rows[0]);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 export async function followUser(req, res) {
-    const {follower_id, followed_id } = req.body;
-    try {
-        const {rowCount} = await selectFollow(follower_id, followed_id)
-        if(rowCount > 0 || followed_id === follower_id) return res.sendStatus(409)
-        
-        await insertFollowUser(follower_id, followed_id)
-        res.send()
-    } catch (error) {
-        res.status(500).send(error)
-    }
+  const { followed_id } = req.params;
+  const { userId: follower_id } = req.locals;
+  const status = req.query.status || false;
+
+  try {
+    const { rowCount } = await selectFollow(follower_id, followed_id);
+
+    if (status && rowCount > 0)
+      return res.send({
+        is_following: true,
+      });
+
+    if (status && rowCount === 0)
+      return res.send({
+        is_following: false,
+      });
+
+    if (rowCount > 0 || followed_id === follower_id) return res.sendStatus(409);
+
+    await insertFollowUser(follower_id, followed_id);
+    res.send("seguindo!");
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 export async function deleteFollow(req, res) {
-    const {follower_id, followed_id } = req.body;
-    try {
-        const {rowCount} = await selectFollow(follower_id, followed_id)
-        if(rowCount === 0) return res.sendStatus(409)
+  const { followed_id } = req.params;
+  const { userId: follower_id } = req.locals;
 
-        await deleteFollowUser(follower_id, followed_id)
-        res.sendStatus(204)
-    } catch (error) {
-        res.status(500).send(error)
-    }
+  try {
+    const { rowCount } = await selectFollow(follower_id, followed_id);
+    if (rowCount === 0) return res.sendStatus(409);
+
+    await deleteFollowUser(follower_id, followed_id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+export async function followStatus(req, res) {
+  const { followed_id } = req.params;
+  const { userId: follower_id } = req.locals;
+
+  try {
+    const { rowCount } = await selectFollow(follower_id, followed_id);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
--- src/schemas/UserSchemas.js
+++
@@ -1,6 +0,0 @@
-import Joi from '@hapi/joi'
-
-export const followSchema = Joi.object({
-    follower_id: Joi.number().required(),
-    followed_id: Joi.number().required()
-})
